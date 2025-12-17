589c606da9ccf052d11b3ee92f6b19a47d1381d3
AmirBazanov <flash5651311@gmail.com>
2025-12-17 21:18:05 +0300

    feat(main): add gracefulshutdown run server in goroutine

--- cmd/bedis/main.go
+++ cmd/bedis/main.go
@@ -3,10 +3,29 @@ package main
 import (
 	"bedis/internal/app"
 	"bedis/internal/config"
+	"bedis/internal/server"
+	"bedis/pkg/logger"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	bedis := app.New(cfg.Server, cfg.Logger)
-	defer bedis.Server.Stop()
+	go func() {
+		err := bedis.Server.Start()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	GracefulShutdown(bedis.Server)
+}
+
+func GracefulShutdown(server *server.Server) {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	sign := <-stop
+	server.Stop()
+	logger.GetLogger().Info("gracefully shutting down", sign.String())
 }
