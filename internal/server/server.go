12a72273fa1b1bf75d8c1bc37606567009713057
AmirBazanov <flash5651311@gmail.com>
2025-12-17 21:20:06 +0300

    feat(main): fix wg errors (Done in wrong location), change bufio scanner to reader, fix logging connection before it

--- internal/server/server.go
+++ internal/server/server.go
@@ -15,49 +15,43 @@ type Server struct {
 	handler  *handler.Handler
 	logger   *slog.Logger
 	listener net.Listener
-	wg       *sync.WaitGroup
+	wg       sync.WaitGroup
 	quit     chan interface{}
 }
 
 func New(address string, handler *handler.Handler, logger *slog.Logger) *Server {
 	op := "server.New"
-
 	if logger == nil {
-		logger = slog.New(
-			slog.NewTextHandler(io.Discard, nil),
-		)
+		logger = slog.New(slog.NewTextHandler(io.Discard, nil))
 	}
-	wait := &sync.WaitGroup{}
 	quit := make(chan interface{})
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatal(err)
 	}
 	logger.Info(op, "Listening on ", address)
-	return &Server{address, handler, logger, listener, wait, quit}
+	return &Server{address: address, handler: handler, logger: logger, listener: listener, quit: quit}
 
 }
 
 func (s *Server) Start() error {
 	op := "server.Start"
-	defer s.wg.Done()
 	for {
 		listener, err := s.listener.Accept()
-		s.logger.Info(op, "Accepting connections on ", s.address)
 		if err != nil {
 			select {
 			case <-s.quit:
 				return nil
 			default:
-				s.logger.Error(op, "Error accepting: ", err.Error())
+				s.logger.Error(op, "Error accepting:", err.Error())
 				continue
 			}
 		}
+		s.logger.Info(op, "Accepting connections on", listener.RemoteAddr().String())
 		s.wg.Add(1)
 		go func() {
-
+			defer s.wg.Done()
 			s.handleConn(listener)
-			s.wg.Done()
 		}()
 	}
 }
@@ -67,18 +61,24 @@ func (s *Server) handleConn(conn net.Conn) {
 	defer func(conn net.Conn) {
 		err := conn.Close()
 		if err != nil {
-			s.logger.Error(op, "Error closing connection: ", err.Error())
+			s.logger.Error(op, "Error closing connection:", err.Error())
 		}
 	}(conn)
-	reader := bufio.NewScanner(conn)
-	for reader.Scan() {
-		line := reader.Text()
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				s.logger.Error(op, "Error reading from connection:", err.Error())
+			}
+			s.logger.Info(op, "Connection closed", conn.RemoteAddr().String())
+			break
+		}
 		resp, err := s.handler.Process(line)
 		if err != nil {
-			s.logger.Error(op, "Error processing: ", err.Error())
+			s.logger.Error(op, "Error processing:", err.Error())
 
 		} else {
-
 			s.logger.Info(op, conn.RemoteAddr().String(), resp)
 		}
 	}
@@ -89,7 +89,7 @@ func (s *Server) Stop() {
 	close(s.quit)
 	err := s.listener.Close()
 	if err != nil {
-		s.logger.Error(op, "Error closing listener: ", err.Error())
+		s.logger.Error(op, "Error closing listener:", err.Error())
 	}
 	s.wg.Wait()
 }
