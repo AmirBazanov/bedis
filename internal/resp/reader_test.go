4191962b1393865dac930d374c0dd1d014912e87
AmirBazanov <flash5651311@gmail.com>
2025-12-27 19:37:30 +0300

    feat(tests): fix test logic and properly rename it

--- internal/resp/reader_test.go
+++ internal/resp/reader_test.go
@@ -5,26 +5,25 @@ import (
 	"testing"
 )
 
-// TODO: Is it right
-func TestArrayReader(t *testing.T) {
+func TestReaderBulkStrings(t *testing.T) {
 	respCmd := "*3\r\n$3\r\nSET\r\n$1\r\nA\r\n$1\r\nB\r\n"
 	buf := bytes.NewBufferString(respCmd)
-
-	value, err := ReadValues(buf)
+	reader := New(buf, nil)
+	value, err := reader.ReadValue()
 	if err != nil {
 		t.Fatalf("error: %s", err)
 	}
 	if len(value.Array) != 3 {
 		t.Fatalf("array of values is wrong size %d", len(value.Array))
 	}
-	if value.Array[0].Type != BulkString && string(value.Array[0].Bytes) != "SET" {
+	if value.Array[0].Type != BulkString || string(value.Array[0].Bytes) != "SET" {
 		t.Fatalf("wrong format or value.Array of first element")
 	}
-	if value.Array[1].Type != BulkString && string(value.Array[1].Bytes) != "A" {
+	if value.Array[1].Type != BulkString || string(value.Array[1].Bytes) != "A" {
 		t.Fatalf("wrong format or value.Array of first element")
 	}
 
-	if value.Array[2].Type != BulkString && string(value.Array[2].Bytes) != "B" {
+	if value.Array[2].Type != BulkString || string(value.Array[2].Bytes) != "B" {
 		t.Fatalf("wrong format or value of first element")
 	}
 }
