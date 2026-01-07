051d611b7082cf771f352536bdb09d78c597f9fc
AmirBazanov <flash5651311@gmail.com>
2026-01-07 20:48:13 +0300

    feat(reader): add tests for integers and bulk strings

--- internal/resp/reader_test.go
+++ internal/resp/reader_test.go
@@ -5,7 +5,19 @@ import (
 	"testing"
 )
 
-func TestReaderBulkStrings(t *testing.T) {
+func TestReaderBulkString(t *testing.T) {
+	respCmd := "$3\r\nASD\r\n"
+	buf := bytes.NewBufferString(respCmd)
+	reader := New(buf, nil)
+	value, err := reader.ReadValue()
+	if err != nil {
+		t.Fatalf("error: %s", err)
+	}
+	if string(value.Bytes) != "ASD" {
+		t.Fatalf("strings don't match: %s", value.Bytes)
+	}
+}
+func TestReaderArrayOfBulk(t *testing.T) {
 	respCmd := "*3\r\n$3\r\nSET\r\n$1\r\nA\r\n$1\r\nB\r\n"
 	buf := bytes.NewBufferString(respCmd)
 	reader := New(buf, nil)
@@ -17,13 +29,27 @@ func TestReaderBulkStrings(t *testing.T) {
 		t.Fatalf("array of values is wrong size %d", len(value.Array))
 	}
 	if value.Array[0].Type != BulkString || string(value.Array[0].Bytes) != "SET" {
-		t.Fatalf("wrong format or value.Array of first element")
+		t.Fatalf("data don't match: %s", value.Array[0].Bytes)
 	}
 	if value.Array[1].Type != BulkString || string(value.Array[1].Bytes) != "A" {
-		t.Fatalf("wrong format or value.Array of first element")
+		t.Fatalf("data don't match: %s", value.Array[1].Bytes)
 	}
 
 	if value.Array[2].Type != BulkString || string(value.Array[2].Bytes) != "B" {
-		t.Fatalf("wrong format or value of first element")
+		t.Fatalf("data don't match: %s", value.Array[2].Bytes)
 	}
+}
+
+func TestReaderInteger(t *testing.T) {
+	respCmd := ":-100\r\n"
+	buf := bytes.NewBufferString(respCmd)
+	reader := New(buf, nil)
+	value, err := reader.ReadValue()
+	if err != nil {
+		t.Fatalf("error: %s", err)
+	}
+	if value.Integer != -100 {
+		t.Fatalf("data don't match: %d", value.Integer)
+	}
+
 }
