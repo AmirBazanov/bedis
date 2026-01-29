2023d2d73f5eafc159b064bed740609957ad5383
AmirBazanov <flash5651311@gmail.com>
2026-01-30 03:14:35 +0500

    add basic test for all types

--- internal/resp/writer_test.go
+++ internal/resp/writer_test.go
@@ -6,16 +6,80 @@ import (
 	"testing"
 )
 
-func TestWriteSimpleString(t *testing.T) {
+func testWrite(t *testing.T, v *Value, cmd string) {
 	var buf bytes.Buffer
+	w := bufio.NewWriter(&buf)
+	writer := NewWriter(w, nil)
+	err := writer.Value(v)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := w.Flush(); err != nil {
+		t.Fatal(err)
+	}
+
+	if cmd != buf.String() {
+		t.Fatalf("Read and Write dont match: %s != %s", cmd, buf.String())
+	}
+
+	t.Logf("OK: %s == %s", cmd, buf.String())
+}
+
+func TestWriteSimpleString(t *testing.T) {
+	cmd := "+HELLO\r\n"
 	v := Value{
 		Type:  SimpleString,
 		Bytes: []byte("HELLO"),
 	}
-	w := bufio.NewWriter(&buf)
-	writer := NewWriter(w, nil)
-	err := writer.WriteValue(&v)
-	if err != nil {
-		t.Error(err)
+
+	testWrite(t, &v, cmd)
+}
+
+func TestWriteInteger(t *testing.T) {
+	cmd := ":10\r\n"
+	v := Value{
+		Type:    Integer,
+		Integer: 10,
 	}
+	testWrite(t, &v, cmd)
+}
+
+func TestWriteSimpleError(t *testing.T) {
+	cmd := "-ERROR\r\n"
+	v := Value{
+		Type:  SimpleError,
+		Bytes: []byte("ERROR"),
+	}
+
+	testWrite(t, &v, cmd)
+}
+
+func TestWriteBulkString(t *testing.T) {
+	cmd := "$5\r\nABCDE\r\n"
+	v := Value{
+		Type:  BulkString,
+		Bytes: []byte("ABCDE"),
+	}
+	testWrite(t, &v, cmd)
+}
+
+func TestWriteArray(t *testing.T) {
+	cmd := "*2\r\n-err\r\n:100\r\n"
+	v := Value{
+		Type: Array,
+		Array: []*Value{
+			{
+				Type:  SimpleError,
+				Bytes: []byte("err"),
+			},
+			{
+				Type:    Integer,
+				Integer: 100,
+			},
+		},
+	}
+
+	testWrite(
+		t, &v, cmd)
 }
