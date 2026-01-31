0ee0f96394167032249d8026e57b60968db9ea0a
AmirBazanov <flash5651311@gmail.com>
2026-02-01 01:13:37 +0300

    increase deadline

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -102,7 +102,7 @@ func TestServerManyCommandsPerConnection(t *testing.T) {
 			t.Errorf("error on client close: %v", err)
 		}
 	}()
-	conn.SetDeadline(time.Now().Add(time.Second))
+	conn.SetDeadline(time.Now().Add(10 * time.Second))
 	client := Client{
 		conn:   conn,
 		reader: bufio.NewReader(conn),
